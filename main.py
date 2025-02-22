b0492fa384f7d6be1cb6f399828a03ee2189ef95
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-02-22 13:43:50 +0530

    Update Dockerfile and main.py to change FastAPI port from 8080 to 8000

--- main.py
+++ main.py
@@ -489,9 +489,9 @@ if __name__ == "__main__":
       "uvicorn",
       "main:app",
       # "--host=localhost",
-      "--port=8080",
+      "--port=8000"
       # f"--workers={workers}",
-      "--reload"
+      # "--reload"
   ]
 
   # uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, workers=workers)
